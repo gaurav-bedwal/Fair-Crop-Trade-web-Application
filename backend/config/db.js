615645a061a6e316547f9e12188e470d0bc19f48
Gaurav Bedwal <gauravbedwal@Gauravs-MacBook-Air.local>
2026-03-17 21:44:02 +0530

    Fix signup bug and add files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/fairtrade");
+  await mongoose.connect("mongodb+srv://gaurav:12345@cluster0.h8u4s0c.mongodb.net/?appName=Cluster0");
   console.log("MongoDB Connected");
 };
 
